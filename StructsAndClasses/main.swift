7734fb1c1bea6ef3abf3137d3268bef4d8e114cd
Artem <atkachuk@uttpic.com>
2020-08-06 13:00:44 -0700

    Initial Commit

---
+++ StructsAndClasses/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StructsAndClasses
+//
+//  Created by Artem Tkachuk on 8/6/20.
+//  Copyright © 2020 Artem Tkachuk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
